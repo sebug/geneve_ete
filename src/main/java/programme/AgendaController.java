57209f3f85b45fe0a7bb386a047c754cee362da3
sebug <sebastian.gfeller@gmail.com>
2015-06-28 13:33:02 +0200

    Now with logging

--- src/main/java/programme/AgendaController.java
+++ src/main/java/programme/AgendaController.java
@@ -6,14 +6,20 @@ import programme.entities.*;
 
 import java.util.List;
 
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+
 @RestController
 @RequestMapping(value="/agenda")
 public class AgendaController {
     @Autowired
     private ProgrammeStoreBean programmeStoreBean;
 
+    private Log log = LogFactory.getLog(AgendaController.class);
+
     @RequestMapping(value="/")
     public List<AgendaEntry> getAllEntries() {
+	log.info("Calling getAllEntries");
 	return this.programmeStoreBean.getAllAgendaEntries();
     }
 }
